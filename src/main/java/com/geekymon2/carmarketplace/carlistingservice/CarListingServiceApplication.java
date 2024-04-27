344bca576352a34d351091ae817f1a1812371981
geekymon2 <geekymon2@gmail.com>
2024-04-27 20:14:14 +1000

    upgraded to used JDK 17

--- src/main/java/com/geekymon2/carmarketplace/carlistingservice/CarListingServiceApplication.java
+++ src/main/java/com/geekymon2/carmarketplace/carlistingservice/CarListingServiceApplication.java
@@ -2,9 +2,7 @@ package com.geekymon2.carmarketplace.carlistingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class CarListingServiceApplication {
 	public static void main(String[] args) {
