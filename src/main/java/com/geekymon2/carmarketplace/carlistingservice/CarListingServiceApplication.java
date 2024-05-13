6287dc4dbafe5561124d8315ac21bf5515c9715a
Geeky Mon2 <geekymon2@gmail.com>
2024-05-13 16:06:51 +1000

    JWT fixes

--- src/main/java/com/geekymon2/carmarketplace/carlistingservice/CarListingServiceApplication.java
+++ src/main/java/com/geekymon2/carmarketplace/carlistingservice/CarListingServiceApplication.java
@@ -3,7 +3,8 @@ package com.geekymon2.carmarketplace.carlistingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.geekymon2.carmarketplace.carlistingservice",
+		"com.geekymon2.carmarketplace.core.autoconfiguration"})
 public class CarListingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarListingServiceApplication.class, args);
